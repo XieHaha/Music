6bca9510037163d2c4d263d5006cbaba9cda0796
dundun <371523912@qq.com>
2019-04-26 10:58:28 +0800

    bug修复 2.1.1.2版本提测

--- app/src/main/java/com/zyc/doctor/ui/adapter/OrderInfoAdapter.java
+++ app/src/main/java/com/zyc/doctor/ui/adapter/OrderInfoAdapter.java
@@ -20,40 +20,34 @@ import org.litepal.crud.DataSupport;
 import java.util.List;
 
 /**
- * Created by dundun on 18/7/14.
+ * @author dundun
+ * @date 18/7/14
  * 患者开单记录适配器
  */
-public class OrderInfoAdapter extends BaseRecyclerAdapter<RegistrationBean> implements OrderStatus
-{
+public class OrderInfoAdapter extends BaseRecyclerAdapter<RegistrationBean> implements OrderStatus {
     private Context context;
 
-    public OrderInfoAdapter(Context context, List<RegistrationBean> list)
-    {
+    public OrderInfoAdapter(Context context, List<RegistrationBean> list) {
         super(list);
         this.context = context;
     }
 
     @Override
-    public BaseViewHolder onCreateViewHolder(ViewGroup parent)
-    {
-        View view = LayoutInflater.from(parent.getContext())
-                                  .inflate(R.layout.item_order_info, parent, false);
-        return new OrderInfoAdapter.Transferholder(view);
+    public BaseViewHolder onCreateViewHolder(ViewGroup parent) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_order_info, parent, false);
+        return new TransferHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(BaseViewHolder holder, int position, RegistrationBean item)
-    {
+    public void onBindViewHolder(BaseViewHolder holder, int position, RegistrationBean item) {
         super.onBindViewHolder(holder, position, item);
         holder.showView(position, item);
     }
 
-    public class Transferholder extends BaseViewHolder<RegistrationBean>
-    {
+    public class TransferHolder extends BaseViewHolder<RegistrationBean> {
         private TextView tvPatientCase, tvCaseType, tvStatus, tvTime;
 
-        public Transferholder(View itemView)
-        {
+        public TransferHolder(View itemView) {
             super(itemView);
             tvTime = itemView.findViewById(R.id.item_order_info_time);
             tvPatientCase = itemView.findViewById(R.id.item_order_info_patient_case);
@@ -62,35 +56,27 @@ public class OrderInfoAdapter extends BaseRecyclerAdapter<RegistrationBean> impl
         }
 
         @Override
-        public void showView(final int position, final RegistrationBean curTransferPatient)
-        {
+        public void showView(final int position, final RegistrationBean curTransferPatient) {
             tvPatientCase.setText(curTransferPatient.getProductName());
-            tvTime.setText(AllUtils.formatDate(curTransferPatient.getOrderDate(),
-                                               AllUtils.YYYY_MM_DD_HH_MM));
-            List<RegistrationTypeBean> list = DataSupport.where("fieldId = ?",
-                                                                curTransferPatient.getProductTypeId())
+            tvTime.setText(AllUtils.formatDate(curTransferPatient.getOrderDate(), AllUtils.YYYY_MM_DD_HH_MM));
+            List<RegistrationTypeBean> list = DataSupport.where("fieldId = ?", curTransferPatient.getProductTypeId())
                                                          .find(RegistrationTypeBean.class);
-            if (list != null && list.size() > 0)
-            {
+            if (list != null && list.size() > 0) {
                 RegistrationTypeBean bean = list.get(0);
                 tvCaseType.setText("#" + bean.getProductTypeName());
             }
-            switch (curTransferPatient.getOrderState())
-            {
+            switch (curTransferPatient.getOrderState()) {
                 case STATUS_SUBSCRIBE_NONE:
                     tvStatus.setText("未确认");
-                    tvStatus.setTextColor(
-                            ContextCompat.getColor(context, R.color.app_main_txt_color));
+                    tvStatus.setTextColor(ContextCompat.getColor(context, R.color.app_main_txt_color));
                     break;
                 case STATUS_SUBSCRIBE:
                     tvStatus.setText("待检查");
-                    tvStatus.setTextColor(
-                            ContextCompat.getColor(context, R.color.app_main_color));
+                    tvStatus.setTextColor(ContextCompat.getColor(context, R.color.app_main_color));
                     break;
                 case STATUS_COMPLETE:
                     tvStatus.setText("已检查");
-                    tvStatus.setTextColor(
-                            ContextCompat.getColor(context, R.color.app_main_txt_color));
+                    tvStatus.setTextColor(ContextCompat.getColor(context, R.color.app_main_txt_color));
                     break;
                 case STATUS_SEND_REPORT:
                     tvStatus.setText("报告已发送");
@@ -98,8 +84,9 @@ public class OrderInfoAdapter extends BaseRecyclerAdapter<RegistrationBean> impl
                     break;
                 case STATUS_REFUSE:
                     tvStatus.setText("已拒绝");
-                    tvStatus.setTextColor(
-                            ContextCompat.getColor(context, R.color.app_red_color));
+                    tvStatus.setTextColor(ContextCompat.getColor(context, R.color.app_red_color));
+                    break;
+                default:
                     break;
             }
         }
